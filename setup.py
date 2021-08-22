443efec1e1ac5f15b49d8bd7e0b4ead6af95ad77
Shawn Brown <shawnbrown@users.noreply.github.com>
2021-08-22 14:07:46 -0400

    Add a clearer error message for missing `distutils` package.
    The Debian Project, starting in version 10 "buster", began shipping
    distributions with a minimal installation of Python. This minimal
    Python install includes the interpreter itself and only those modules
    necessary for other essential system tasks. Many other down-stream
    distributions like Ubuntu, Mint, MX Linux, etc. have inherited this
    behavior as well.

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 """A package to demonstrate "scripts" and "data_files" installation."""
-from distutils.core import setup
+try:
+    from distutils.core import setup
+except ModuleNotFoundError as error:
+    error.msg = f"""{error.msg}
+
+    Your system does not appear to have the full `distutils` package.
+    Distutils is part of the Python Standard Library but some Linux
+    distributions are shipped with a minimal install of Python. You
+    may have to install `distutils` using your system's package manager.
+
+    For Debian or Debian-based distributions (like Ubuntu, Mint, etc.):
+
+        sudo apt install python3-distutils
+    """
+    raise error
 
 
 setup(
